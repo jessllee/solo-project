e517351750c7c1872fda0eb36bff5c6c5038de3a
Jessica <jlouielee@gmail.com>
2022-01-26 15:41:34 -0800

    Trying to get frontend checkboxes and submit to send info back to backend.

--- App.jsx
+++ App.jsx
@@ -1,18 +1,42 @@
-import React, { Component } from 'react';
+import React, { Component, useReducer, useState } from 'react';
 import { render } from 'react-dom';
 import { Brew, Dairy, Else } from './headers.jsx';
 
 // import './stylesheets/styles.css';
 
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.id]: event.value
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
   render() {
+
+    const [formData, setFormData] = useReducer(formReducer, {})
+    const [submitting, setSubmitting] = useState(false)
+
+    const handleSubmit = event => {
+      event.preventDefault()
+      setSubmitting(true)
+
+      setTimeout(() => {
+        setSubmitting(false)
+      }, 3000)
+    }
+
     return (
       <div className='selection'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <Brew />
           <Dairy />
           <Else />
-          <input type='submit' value='What is My Drink?' />
+          <button type='submit'>What is My Drink?</button>
         </form>
       </div>
     )
--- headers.jsx
+++ headers.jsx
@@ -1,53 +1,78 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+const handleChange = event => {
+  setFormData({
+    [event.target.name]: true
+  })
+}
+
 class Brew extends Component {
   render() {
-    return (<div>Pick a brew.
-      <br></br>
-      <input type='checkbox'/>
-      <label>Coffee</label>
-      <input type='checkbox'/>
-      <label>Espresso</label>
-    </div>)
+    return (
+      <fieldset>
+        <legend>Pick a brew.</legend>
+
+        <input type='checkbox' name='coffee' onChange={handleChange}/>
+        <label for='coffee'>Coffee</label>
+
+        <input type='checkbox' name='espresso' onChange={handleChange}/>
+        <label for='espresso'>Espresso</label>
+      </fieldset>
+    )
   }
 }
 
 class Dairy extends Component {
   render() {
-    return (<div>Any dairy?
-      <br></br>
-      <input type='checkbox'/>
-      <label>Steamed Milk</label>
-      <input type='checkbox'/>
-      <label>Frothed Milk</label>
-      <input type='checkbox'/>
-      <label>Foamed Milk</label>
-      <input type='checkbox'/>
-      <label>Condensed Milk</label>
-      <input type='checkbox'/>
-      <label>Cream</label>
-      <input type='checkbox'/>
-      <label>Whipped Cream</label>
-      <input type='checkbox'/>
-      <label>Ice Cream</label>
-    </div>)
+    return (
+      <fieldset>
+        <legend>Any dairy?</legend>
+
+        <input type='checkbox' name='steamed milk' onChange={handleChange}/>
+        <label for='steamed milk'>Steamed Milk</label>
+
+        <input type='checkbox' name='frothed milk' onChange={handleChange}/>
+        <label for='frothed milk'>Frothed Milk</label>
+
+        <input type='checkbox' name='foamed milk' onChange={handleChange}/>
+        <label for='foamed milk'>Foamed Milk</label>
+
+        <input type='checkbox' name='condensed milk' onChange={handleChange}/>
+        <label for='condensed milk'>Condensed Milk</label>
+
+        <input type='checkbox' name='cream' onChange={handleChange}/>
+        <label for='cream'>Cream</label>
+
+        <input type='checkbox' name='whipped cream' onChange={handleChange}/>
+        <label form='whippedcream'>Whipped Cream</label>
+
+        <input type='checkbox' name='ice cream' onChange={handleChange}/>
+        <label for= 'ice cream'>Ice Cream</label>
+      </fieldset>
+    )
   }
 }
 
 class Else extends Component {
   render() {
-    return (<div>What else?
-      <br></br>
-      <input type='checkbox'/>
-      <label>Sugar</label>
-      <input type='checkbox'/>
-      <label>Alcohol</label>
-      <input type='checkbox'/>
-      <label>Water</label>
-      <input type='checkbox'/>
-      <label>Chocolate</label>
-    </div>)
+    return (
+      <fieldset>
+        <legend>What else?</legend>
+
+        <input type='checkbox' name='sugar'/>
+        <label for='sugar'>Sugar</label>
+
+        <input type='checkbox' name='alcohol'/>
+        <label for='alcohol'>Alcohol</label>
+
+        <input type='checkbox' name='water'/>
+        <label for='water'>Water</label>
+
+        <input type='checkbox' name='chocolate'/>
+        <label for='chocolate'>Chocolate</label>
+      </fieldset>
+    )
   }
 }
 
